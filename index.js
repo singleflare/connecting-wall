fde7863130164b6ef20d7ce7b9cb4ec0cb1c4653
singleflare <dodinhdong28092002@gmail.com>
2023-09-13 09:54:22 +0700

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+let groups=[
+  {link:"1",clues:["1","2","3","4"]},
+  {link:"2",clues:["5","6","7","8"]},
+  {link:"3",clues:["9","10","11","12"]},
+  {link:"4",clues:["13","14","15","16"]}
+]
+groups.forEach(group => {
+  group.clues.forEach(clue => {
+    $('wall').html(clue)
+  });
+});
