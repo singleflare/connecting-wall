f9c78d5c257a46b1c9be30e268a56b910913847f
singleflare <dodinhdong28092002@gmail.com>
2023-09-18 22:42:38 +0700

    hover

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 let groups=[
-  {link:"1",clues:["1","2","3","4"]},
-  {link:"2",clues:["5","6","7","8"]},
-  {link:"3",clues:["9","10","11","12"]},
-  {link:"4",clues:["13","14","15","16"]}
+  {group1:["1","2","3","4"]},
+  {group2:["5","6","7","8"]},
+  {group3:["9","10","11","12"]},
+  {group4:["13","14","15","16"]}
 ]
-for(var i=0;i<=15;i++){
-  document.getElementsByClassName('brick')[i].addEventListener("click",()=>{console.log('click')})
+let brickClues=[]
+for(let i=0;i<3;i++){
+  for(let j=0;j<3;j++){
+    brickClues.push(groups.at(i).group1[j])
+    brickClues.push(groups.at(i).group2[j])
+    brickClues.push(groups.at(i).group3[j])
+    brickClues.push(groups.at(i).group4[j])
+  }
+}
+console.log(brickClues)
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const WIDTH=4
+let selected=[]
+function selectBrick(brickNum){
+  selected.push(brickNum)
 }
