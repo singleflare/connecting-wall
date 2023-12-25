73dddbadad5ca6df0b181db38b5dda01db88c754
singleflare <dodinhdong28092002@gmail.com>
2023-12-25 11:40:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let groupsAndClues=[
 function playAudio(audio){
   new Audio('./audio/'+audio).play()
 }
-let selectedClueElementsList=[]
+let selectedClueEList=[]
 let correctGroups=0
 
 let cluesListList=(()=>{
@@ -41,44 +41,42 @@ function createHtml(parent,tag,cssClass,content){
   return child
 }
 
-function select(brickElement){
+function select(brickE){
   playAudio('wallBtnClick.mp3')
-  selectedClueElementsList.push(brickElement)
-  console.log(selectedClueElementsList)
-  brickElement.style.backgroundColor='#054872'
-  brickElement.style.color='white'
-  if(selectedClueElementsList.length==4){setTimeout(checkCorrect,500)}
+  selectedClueEList.push(brickE)
+  console.log(selectedClueEList)
+  brickE.style.backgroundColor='#054872'
+  brickE.style.color='white'
+  if(selectedClueEList.length==4){setTimeout(checkCorrect,500)}
 }
 
-function deselect(brickElement){
+function deselect(brickE){
   
 }
 
-let listOfCluesList=(()=>{
-  let result=[]
-  groupsAndClues.forEach(group=>{result.push(...Object.values(group))})
-  return result
-})
-
 function checkCorrect(){
   cluesListList.forEach(function(cluesList){
-    let selectedCluesList=(()=>{
+    let selectedClueList=(()=>{
       let res=[]
-      selectedClueElementsList.forEach(function(selectedClueElement){res.push(selectedClueElement.innerHTML)})
+      selectedClueEList.forEach(function(selectedClueE){res.push(selectedClueE.innerHTML)})
       return res
     })()
     let sortedCluesList=cluesList.sort()
-    let sortedSelectedCluesList=selectedCluesList.sort()
+    let sortedSelectedCluesList=selectedClueList.sort()
     if(sortedSelectedCluesList.every((clue,i)=>clue===sortedCluesList[i])){
       playAudio('solveClue.mp3')
     }
   })
   playAudio('incorrectGroup.mp3')
-  selectedClueElementsList.forEach((element)=>{
+  selectedClueEList.forEach((element)=>{
     element.style.backgroundColor='#91C3E4'
     element.style.color='black'
   })
-  selectedClueElementsList=[]
+  selectedClueEList=[]
+}
+
+function move(el1,el2){
+  
 }
 
 function game(){
